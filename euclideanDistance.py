fe40b44f593dddb22527f116c27aa5f6befa8016
Nurettin Kaplan <nurettinkaplaan@gmail.com>
2024-05-16 21:37:37 +0300

    Euclidean calculation exercise was done.

---
+++ euclideanDistance.py
@@ -0,0 +1,14 @@
+import math
+def euclideanDistance(point1, point2):
+    distance = ((point1[0] - point2[0]) ** 2) + ((point2[1] - point2[1]) ** 2)
+    return math.sqrt(distance)
+
+points = [(1,2), (8,9), (4,5), (10,7)]
+
+distances = []
+
+for i in range(len(points) - 1):
+    for j in range(i + 1, len(points)):
+        distances.append(euclideanDistance(points[i], points[j]))
+
+print(min(distances))
